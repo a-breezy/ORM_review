82ec89cb8a5bb7378b31f18fcb6e362c4b58d6f7
a.breezy <ambrose.wilkins@gmail.com>
2023-02-01 11:23:06 -0500

    created upvote method on Post model and updated routes in user and post to reflect change

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Vote } = require("../../models");
 
 router.get("/", (req, res) => {
 	User.findAll({ attributes: { exclude: ["password"] } })
@@ -12,10 +12,22 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
 	User.findOne({
-		// attributes: { exclude: ["password"] },
+		attributes: { exclude: ["password"] },
 		where: {
 			id: req.params.id,
 		},
+		include: [
+			{
+				model: Post,
+				attributes: ["id", "title", "post_url", "created_at"],
+			},
+			{
+				model: Post,
+				attributes: ["title"],
+				through: Vote,
+				as: "voted_posts",
+			},
+		],
 	})
 		.then((dbUserData) => {
 			if (!dbUserData) {
